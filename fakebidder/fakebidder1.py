4f39f42d91b95788b28b1fef83e75269e13f9492
km <mahant.kapil@gmail.com>
2015-08-01 20:30:56 -0700

    with getting value from cookie

--- fakebidder/fakebidder1.py
+++ fakebidder/fakebidder1.py
@@ -5,11 +5,43 @@ import os
 import bottle
 import random 
 
-@bottle.route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
 
 
+##########
+# Global variable 
+#########
+
+#global auction_id
+
+########## 
+# Functions , TODO :  Will be moved to a module 
+##########
+
+
+# TODO : return it as a dictionary and not a string 
+def dump_headers():
+    headersDict = {}
+    headersDict = bottle.request.headers
+    result = ["%s: %s" % (x, bottle.request.headers[x]) for x in bottle.request.headers.keys()]
+    #print bottle.request.get_header('host')
+    #print bottle.request.headers.keys()
+    #print bottle.request.headers['host']
+    return result 
+
+# TODO : return it as a dictionary and not a string 
+def dump_cookies():
+    result = ["%s: %s" % (x, bottle.request.cookies[x]) for x in bottle.request.cookies.keys()]
+    result.append('\n')
+    #print bottle.request.cookies.keys()
+    #print bottle.request.get_cookie("cookieName")
+    return result
+
+
+# TODO : return it as a dictionary and not a string 
+def dump_data():
+    result = bottle.request.body.read() 
+    return result
+ 
 def is_valid_json(jsonVariable):
     try:
         json_object = json.loads(jsonVariable) 
@@ -19,11 +51,18 @@ def is_valid_json(jsonVariable):
 
 
 def get_bid_template():
-
     # get random bid amount just in case not provided 
     # var bid = parseFloat(parseInt(Math.random()*10) + "." + parseInt(Math.random()*100000));
 
-    bid = random.uniform(1, 10)
+    bid = 0 
+    print bottle.request.get_cookie("returnBidAmount")
+
+    #print bottle.request.get_cookie("cookieName")
+    if (  bottle.request.get_cookie("returnBidAmount") ) :
+        bid = bottle.request.get_cookie("returnBidAmount")
+        print bid 
+    else :
+        bid = random.uniform(1, 10)
 
     responseJson = {
            "id": "auctionId",
@@ -43,31 +82,36 @@ def get_bid_template():
     return responseJson
 
 
-
 def populate_bid_template(requestData, responseJson):
-
-    # print requestData.keys()  
+    global auction_id
     requestDataJson = json.loads(requestData) 
     auction_id =  requestDataJson['auction_id']  
-    print auction_id 
-    #return auction_id 
-    #responseDataJson = json.loads(responseJson) 
-    #print responseDataJson['id']
-    print responseJson
     responseJson['id'] = auction_id    
-    print requestData 
-    print responseJson
     return responseJson
 
 
+#############
+# End points 
+############
+
+@bottle.route('/hello/:name')
+def index(name='World'):
+    return '<b>Hello %s!</b>' % name
 
 
+@bottle.route('/requestdetails', method='POST')
+def print_request_details():
+    requestDetails = {}
+    requestDetails['headers'] = {}
+    requestDetails['headers'] =  dump_headers()
+    requestDetails['cookies'] =  dump_cookies()
+    requestDetails['data']    =  dump_data()
+    bottle.response.content_type = 'application/json'
+    return requestDetails 
+   
 
 @bottle.route('/fakebidder/', method='POST')
-def recipe_save() :
-    print "got something"
-  
-
+def return_bid() :
     requestData = bottle.request.body.read()
 
     #check if valid json 
@@ -77,12 +121,11 @@ def recipe_save() :
         return "ERROR: bad JSON"
      
     responseJson = get_bid_template()
-
     responseBid = populate_bid_template(requestData,responseJson) 
-
+    
+    print "DSP Response : id : ", auction_id ," : " , responseBid
 
     return responseBid
-    #return postdata
 
 
 
@@ -92,12 +135,4 @@ def recipe_save() :
 
 
 bottle.run(host='0.0.0.0', port=10001)
-
-
-
-
-# todo 
-# insert everything in try / catch 
-# check for json 
-# 
 
