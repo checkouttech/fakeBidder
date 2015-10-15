8d6335805ab61791df80ba7a7aa940f79727b2f7
km <mahant.kapil@gmail.com>
2015-10-14 21:12:15 -0700

    with module integration

--- fakebidder/fakebidder1.py
+++ fakebidder/fakebidder1.py
@@ -6,85 +6,19 @@ import bottle
 import random 
 import sys
 import logging
+import fakebidder 
+from fakebidder import *
 
 
 ##########
 # Global variable 
 #########
 
-#global auction_id
+global auction_id
 
 ########## 
 # Functions , TODO :  Will be moved to a module 
 ##########
-
-
-def dump_headers():
-    headersDict = {}
-    headersDict = bottle.request.headers
-    #print bottle.request.get_header('host')
-    #print bottle.request.headers.keys()
-    #print bottle.request.headers['host']
-    return dict ( headersDict ) 
-
-def dump_cookies():
-    cookiesDict = {}
-    cookiesDict = bottle.request.cookies
-    #print bottle.request.cookies.keys()
-    #print bottle.request.get_cookie("cookieName")
-    return dict ( cookiesDict)
-
-def dump_data():
-    dataDict = {}
-    dataDict =  json.loads ( bottle.request.body.read() ) 
-    return dataDict 
-
-def is_valid_json(jsonVariable):
-    try:
-        json_object = json.loads(jsonVariable) 
-    except ValueError, e:
-        return False
-    return True 
-
-
-def get_bid_template():
-    # get random bid amount just in case not provided 
-
-    bid = 0 
-    print bottle.request.get_cookie("returnBidAmount")
-
-    #print bottle.request.get_cookie("cookieName")
-    if (  bottle.request.get_cookie("returnBidAmount") ) :
-        bid = bottle.request.get_cookie("returnBidAmount")
-        print bid 
-    else :
-        bid = random.uniform(1, 10)
-
-    responseJson = {
-           "id": "auctionId",
-           "seatbid": [ {
-                 "seat":"testseat",
-                 "bid": [ {
-                         "id": "required id per 4.3.3",
-                         "impid": "impid per 4.3.3",
-                         "adomain": [ "gawker" ],
-                         "price": bid,
-                         "ext": { "creativeapi": 3 },
-                         "adm": "<img src=\"http://www.foo.com/wp-content/themes/foo/images/ftr_icon.png?auction=${AUCTION_ID}&price=${AUCTION_PRICE:BF}\">",
-                         "crid": "12345678"
-                        } ]
-                      } ]
-         }
-    return responseJson
-
-
-def populate_bid_template(requestData, responseJson):
-    global auction_id
-    requestDataJson = json.loads(requestData) 
-    auction_id =  requestDataJson['auction_id']  
-    responseJson['id'] = auction_id    
-    return responseJson
-
 
 #############
 # End points 
@@ -124,12 +58,12 @@ def return_bid() :
     responseJson = get_bid_template()
     responseBid = populate_bid_template(requestData,responseJson) 
     
-    print2log = "DSP Response : id : ", auction_id ," : " , responseBid
+    print2log = "DSP Response : id : ", fakebidder.auction_id ," : " , responseBid
 
 
     logging.info(print2log)
     sys.stdout.flush()
-    print "DSP Response : id : ", auction_id ," : " , responseBid
+    print "DSP Response : id : ", fakebidder.auction_id ," : " , responseBid
 
     return responseBid
 
@@ -145,6 +79,7 @@ cmdargs = sys.argv
 PORT_NUMBER = cmdargs[1] 
 print "\\\\\\\\\\\\\\==================================="
 print PORT_NUMBER 
+print fakebidder.hello()
 
 bottle.run(host='0.0.0.0', port=PORT_NUMBER)
 #bottle.run(host='0.0.0.0', port=10001)
