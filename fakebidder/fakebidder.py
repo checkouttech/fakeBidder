45debaeeb0215a0adce342a806e94189dbd06d3e
Kapil Mahant <kmahant@la-mac-kmahant2.rubicorp.com>
2015-11-06 12:12:20 -0800

    moved stuff to class

--- fakebidder/fakebidder.py
+++ fakebidder/fakebidder.py
@@ -60,11 +60,6 @@ class requestClass():
         print type ( requestDetails )
         #print  dict ( requestDetails ,skipkeys = True ,ensure_ascii=False, sort_keys=True )
 
-        #bottle.response.content_type = 'application/json'
-        #return json.dumps ( requestDetails )
-         #self.requestData = bottle.request.body.read()
-        #self.requestData = bottleRequestObject.body.read()
-    
         #check if valid json
         #if ( is_valid_json(requestData)) :
         #    postdata = bottle.request.body.read()
@@ -72,11 +67,6 @@ class requestClass():
         #    return "ERROR: bad JSON"
         #pass
    
-        #headersDict = bottle.request.headers
-        #headersDict = self.requestObject.headers
-        #print bottle.request.get_header('host')
-        #print bottle.request.headers.keys()
-        #print bottle.request.headers['host']
   
         return requestDetails
 
@@ -121,36 +111,15 @@ class responseClass():
                           } ]
              }
         return self.bidResponseTemplate
-    
+
 
     def createResponse(self,bidrequest):
 
-        print "inside createResponse -----> ", bidrequest.requestDetails['data']
-
-    #     responseBid = populate_bid_template(bidrequest.requestDetails['data'] , bidresponse.get_bid_template())
-
-        #def populate_bid_template(requestData, responseJsonTemplate):
-        
-        # populate auction_id from request 
-        # get auction id from bid request and set it in response
-
-        print " auction id from requeset :::::::::::::::::> ",  bidrequest.requestDetails['data']['auction_id']
-
         # populate bid price 
-        print " bid price from cookie :::::::::::::::::> ",  bidrequest.requestDetails['cookies']['returnBidAmount'] 
- 
-        # bid response template 
-        print "bid response template:::::::::::::::::::> ",   self.bidResponseTemplate
-
-
-
         if ( 'userID' in bidrequest.requestDetails['data'] ) :
             # TODO : if network-userID key present in KV
             userID = bidrequest.requestDetails['data']['userID']
             userKV = memobj.getValue(userID)
-
-            print "userID presenent -------> " , bidrequest.requestDetails['data']['userID'] 
-            print " user ID value ", userKV
             bid = userKV['returnBidAmount']
 
         elif ( bidrequest.requestDetails['cookies']['returnBidAmount'] ) : # if return bid price present in bid request cookie , use it
@@ -159,136 +128,47 @@ class responseClass():
         else :  # return randon bid price
             bid = random.uniform(1, 10)
 
-        print "bid amount =------> " , bid
-
+        # bid response template 
         self.bidResponse  = self.bidResponseTemplate
     
+        # get auction id from bid request and set it in response
+        self.bidResponse['id'] = bidrequest.requestDetails['data']['auction_id']
 
-
+        # set the bid in response 
         self.bidResponse['seatbid'][0]['bid'][0]['price'] = bid 
 
 
-        print "self.response -----> " , self.bidResponse
+def mycustomelogger():
 
-#%            # set the bid in response 
-#%            responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
-#      
-#%            responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
-#%            responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
- 
-
-
- 
-#%          
-#%            elif (  bottle.request.get_cookie("returnBidAmount") ) :  # if return bid price present in bid request cookie , use it
-#%                bid = bottle.request.get_cookie("returnBidAmount")
-#%        
-#%            else :  # return randon bid price  
-#%                bid = random.uniform(1, 10)
-#%         
-#%            # set the bid in response 
-#%            responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
-#%   
-
-
-
-#%            # popupate bid price 
-#%            bid = 0 
-#%            print bottle.request.get_cookie("returnBidAmount")
-        
- 
-#%        responseJsonTemplate['id'] = auction_id 
-#% 
-#%
-#%        self.requestDetails['data']    =  json.loads ( bottle.request.body.read() ) 
-#%
-#%            global auction_id
-#%            
-#%            # convert json to a dictionary 
-#%            ### requestDataDict = json.loads(requestData) 
-#%            requestDataDict = requestData
-#%        
-#%       
-#%        
-#%        
-#%            if ( 'userID' in requestDataDict ) :  # if request contains userID 
-#%                # TODO : if network-userID key present in KV
-#%                userID = requestDataDict['userID']
-#%                userKV = memobj.getValue(userID)
-#%                bid = userKV['returnBidAmount']
-#%          
-#%            elif (  bottle.request.get_cookie("returnBidAmount") ) :  # if return bid price present in bid request cookie , use it
-#%                bid = bottle.request.get_cookie("returnBidAmount")
-#%        
-#%            else :  # return randon bid price  
-#%                bid = random.uniform(1, 10)
-#%         
-#%            # set the bid in response 
-#%            responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
-#%         
-#%            return responseJsonTemplate
-#%        
-
-
-
-    #bidresponse.createResponse(bidrequest)
-
-
-
-#class myLogger():
-#
-#    def __init__(self,logging.getLoggerClass()):
-#        self.logger = logging.getLogger('custom_logger')
-#            
-#    def setup_custom_logger(name):
-#        formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-#    
-#        handler = logging.StreamHandler()
-#        handler.setFormatter(formatter)
-#    
-#        logger = logging.getLogger(name)
-#        logger.setLevel(logging.DEBUG)
-#        logger.addHandler(handler)
-#        return logger
-#    
-#
-#    def setup_logger(logger_name, log_file, level=logging.INFO):
-#        l = logging.getLogger(logger_name)
-#        formatter = logging.Formatter('%(asctime)s : %(message)s')
-#        fileHandler = logging.FileHandler(log_file, mode='w')
-#        fileHandler.setFormatter(formatter)
-#        streamHandler = logging.StreamHandler()
-#        streamHandler.setFormatter(formatter)
-#    
-#        l.setLevel(level)
-#        l.addHandler(fileHandler)
-#        l.addHandler(streamHandler)   
-#    
-
-       #logging.basicConfig(  stream=sys.stdout, 
-       #                       level=logging.INFO,
-       #                       format= '[%(asctime)s %(levelname)s] {%(pathname)s:%(lineno)d} - %(message)s',
-       #                       datefmt='%H:%M:%S')
-       # logging.info('Watch out!')
-        
+    #create logger with "spam_application"
+    logger = logging.getLogger("spam_application")
+    logger.setLevel(logging.DEBUG)
     
-
-
-
-
-
-
-
+    #create file handler and set level to debug
+    fh = logging.FileHandler("spam.log")
+    fh.setLevel(logging.DEBUG)
+    
+    #create formatter
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s -  %(message)s")
+    #add formatter to fh
+    fh.setFormatter(formatter)
+    
+    #add fh to logger
+    logger.addHandler(fh)
+    
+    logging.basicConfig(  stream=sys.stdout, 
+                      level=logging.INFO,
+                      format= '[%(asctime)s %(levelname)s] {%(pathname)s:%(lineno)d} - %(message)s',
+                      datefmt='%H:%M:%S')
+    
+    return logging 
+    #return logger 
+    
 
 ## Declaring global objects
 global memobj 
 memobj = memcacheConnection() 
  
-
-
-
-
-
 
 
 def hello():
@@ -303,92 +183,61 @@ def is_valid_json(jsonVariable):
         return False
     return True 
 
-
-#
-#def dump_headers():
-#    headersDict = {}
-#    headersDict = bottle.request.headers
-#    #print bottle.request.get_header('host')
-#    #print bottle.request.headers.keys()
-#    #print bottle.request.headers['host']
-#    return dict ( headersDict ) 
-#
-#def dump_cookies():
-#    cookiesDict = {}
-#    cookiesDict = bottle.request.cookies
-#    #print bottle.request.cookies.keys()
-#    #print bottle.request.get_cookie("cookieName")
-#    return dict ( cookiesDict)
-#
-#def dump_data():
-#    dataDict = {}
-#    dataDict =  json.loads ( bottle.request.body.read() ) 
-#    return dataDict 
-#
-def get_bid_template():
-    #  will eventually come from a yaml template file 
-    bidResponseTemplate  = {
-           "id": "auctionId",
-           "seatbid": [ {
-                 "seat":"testseat",
-                 "bid": [ {
-                         "id": "required id per 4.3.3",
-                         "impid": "impid per 4.3.3",
-                         "adomain": [ "gawker" ],
-                         "price": "BID_PRICE",
-                         "ext": { "creativeapi": 3 },
-                         "adm": "<img src=\"http://www.foo.com/wp-content/themes/foo/images/ftr_icon.png?auction=${AUCTION_ID}&price=${AUCTION_PRICE:BF}\">",
-                         "crid": "12345678"
-                        } ]
-                      } ]
-         }
-    return bidResponseTemplate
-
-def populate_bid_template(requestData, responseJsonTemplate):
-    global auction_id
-    
-    # convert json to a dictionary 
-    ### requestDataDict = json.loads(requestData) 
-    requestDataDict = requestData
-
-    # get auction id from bid request and set it in response
-    auction_id =  requestDataDict['auction_id']  
-    responseJsonTemplate['id'] = auction_id 
-
-    # popupate bid price 
-    bid = 0 
-    print bottle.request.get_cookie("returnBidAmount")
-
-
-    if ( 'userID' in requestDataDict ) :  # if request contains userID 
-        # TODO : if network-userID key present in KV
-        userID = requestDataDict['userID']
-        userKV = memobj.getValue(userID)
-        bid = userKV['returnBidAmount']
-  
-    elif (  bottle.request.get_cookie("returnBidAmount") ) :  # if return bid price present in bid request cookie , use it
-        bid = bottle.request.get_cookie("returnBidAmount")
-
-    else :  # return randon bid price  
-        bid = random.uniform(1, 10)
- 
-    # set the bid in response 
-    responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
- 
-    return responseJsonTemplate
-
-
+#%
+#%def get_bid_template():
+#%    #  will eventually come from a yaml template file 
+#%    bidResponseTemplate  = {
+#%           "id": "auctionId",
+#%           "seatbid": [ {
+#%                 "seat":"testseat",
+#%                 "bid": [ {
+#%                         "id": "required id per 4.3.3",
+#%                         "impid": "impid per 4.3.3",
+#%                         "adomain": [ "gawker" ],
+#%                         "price": "BID_PRICE",
+#%                         "ext": { "creativeapi": 3 },
+#%                         "adm": "<img src=\"http://www.foo.com/wp-content/themes/foo/images/ftr_icon.png?auction=${AUCTION_ID}&price=${AUCTION_PRICE:BF}\">",
+#%                         "crid": "12345678"
+#%                        } ]
+#%                      } ]
+#%         }
+#%    return bidResponseTemplate
+#%
+#%def populate_bid_template(requestData, responseJsonTemplate):
+#%    global auction_id
+#%    
+#%    # convert json to a dictionary 
+#%    ### requestDataDict = json.loads(requestData) 
+#%    requestDataDict = requestData
+#%
+#%    # get auction id from bid request and set it in response
+#%    auction_id =  requestDataDict['auction_id']  
+#%    responseJsonTemplate['id'] = auction_id 
+#%
+#%    # popupate bid price 
+#%    bid = 0 
+#%    print bottle.request.get_cookie("returnBidAmount")
+#%
+#%
+#%    if ( 'userID' in requestDataDict ) :  # if request contains userID 
+#%        # TODO : if network-userID key present in KV
+#%        userID = requestDataDict['userID']
+#%        userKV = memobj.getValue(userID)
+#%        bid = userKV['returnBidAmount']
+#%  
+#%    elif (  bottle.request.get_cookie("returnBidAmount") ) :  # if return bid price present in bid request cookie , use it
+#%        bid = bottle.request.get_cookie("returnBidAmount")
+#%
+#%    else :  # return randon bid price  
+#%        bid = random.uniform(1, 10)
+#% 
+#%    # set the bid in response 
+#%    responseJsonTemplate['seatbid'][0]['bid'][0]['price'] = bid 
+#% 
+#%    return responseJsonTemplate
+#%
+#%
 
 def ifBuyerIdPresent ():
     pass 
-     
-#     responseBid = populate_bid_template(requestData,responseJson)
-
-    # convert json to dictionary 
-    # responseJsonTemplateDict = json.loads(responseJsonTemplate) 
-
-    #print "is NOT instance of dict ",    isinstance(responseJsonTemplate, dict) 
-    #print "is instance of dict ",    isinstance(responseJsonTemplate, dict) 
-
-
 
--- fakebidder/fakebidder1.py
+++ fakebidder/fakebidder1.py
@@ -5,7 +5,7 @@ import os
 import bottle
 import random 
 import sys
-import logging
+#import logging
 import fakebidder 
 from fakebidder import *
 
@@ -14,7 +14,7 @@ from fakebidder import *
 # Global variable 
 #########
 
-global auction_id
+#global auction_id
 
 #############
 # End points 
@@ -48,74 +48,27 @@ def return_bid() :
 
     bidresponse.get_bid_template()    # TODO : move into bid template 
     bidresponse.createResponse(bidrequest)
-    
+
+    mylogger = mycustomelogger()
+    mylogger.info(bidresponse.bidResponse) 
+   
     return json.dumps ( bidresponse.bidResponse ) 
 
 
-#%
-#%    print2log = "DSP Response : id : ", fakebidder.auction_id ," : " , responseBid
-#%
-#%    logging.info(print2log)
-#%    sys.stdout.flush()
-#%    print "DSP Response : id : ", fakebidder.auction_id ," : " , responseBid
-#%
-#%    #logger = loggingClass()
-#%
-#%    bidresponse.set_response(responseBid) 
-#%
-#%    bidresponse.content_type = 'application/json'
-#%    return json.dumps ( bidresponse.get_response() )
-
-    #requestDetails =  bidrequest.getDetails()
-    #requestData = json.dumps ( requestDetails['data'] ) 
-
-    #bidresponse.set_response(requestDetails) 
-
-    #print " print the object ------> " , bidrequest.printRequest()
-
-    #print "getDetails function ---->   " , bidrequest.getDetails()
-    #print "requestDetails variable ----> " , bidrequest.requestDetails 
-
-    #requestData = bottle.request.body.read()
-
-    #print "requestData direct " , requestData
-
-    #check if valid json 
-    #if ( is_valid_json(requestData)) :
-    #    postdata = bottle.request.body.read()
-    #else:
-    #    return "ERROR: bad JSON"
-     
-    #if ( bidrequest.is_valid_data(requestDetails['data'])):
-    #    print  "valid json " 
-    #else :
-    #     return "ERROR: bad JSON"
-
-    #responseBidTemplate = get_bid_template()
-
-    #responseBidTemplate = bidresponse.get_bid_template()
-
-    #responseBid = populate_bid_template(requestData,responseBidTemplate) 
-    #responseBid = populate_bid_template(requestDetails['data'] , bidresponse.get_bid_template()) 
-    #return responseBid
 
 
-   
-logging.basicConfig(  stream=sys.stdout, 
-                      level=logging.INFO,
-                      format= '[%(asctime)s %(levelname)s] {%(pathname)s:%(lineno)d} - %(message)s',
-                      datefmt='%H:%M:%S')
-logging.info('Watch out!')
-#logging.basicConfig(format=FORMAT,level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
-# format='%(asctime)s %(levelname)s %(message)s',
-cmdargs = sys.argv
+def main():
+    cmdargs = sys.argv
+    PORT_NUMBER = cmdargs[1] 
+    print "\\\\\\\\\\\\\\==================================="
+    print PORT_NUMBER 
 
-PORT_NUMBER = cmdargs[1] 
-print "\\\\\\\\\\\\\\==================================="
-print PORT_NUMBER 
-print fakebidder.hello()
+    bottle.run(host='0.0.0.0', port=PORT_NUMBER)
+    # my code here
 
-bottle.run(host='0.0.0.0', port=PORT_NUMBER)
-#bottle.run(host='0.0.0.0', port=10001)
+if __name__ == "__main__":
+    main()
 
-    #print  dict ( requestDetails ,skipkeys = True ,ensure_ascii=False, sort_keys=True )
+
+  
+
